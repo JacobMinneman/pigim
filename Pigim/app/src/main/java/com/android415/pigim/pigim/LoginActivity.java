50f283a3e2ae7907722f9af898b2d6639d56cc5f
reiterj1git <reiterj1@nku.edu>
2018-10-29 11:36:51 -0400

    LOGIN SCREEN CHANGES Added checks to make sure username was email form Changed input type of username to email text added a show password checkbox

--- Pigim/app/src/main/java/com/android415/pigim/pigim/LoginActivity.java
+++ Pigim/app/src/main/java/com/android415/pigim/pigim/LoginActivity.java
@@ -5,13 +5,18 @@ import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatDelegate;
+import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.security.PrivateKey;
 import java.util.PriorityQueue;
+import java.util.regex.Pattern;
 
 public class LoginActivity extends AppCompatActivity
 {
@@ -22,7 +27,7 @@ public class LoginActivity extends AppCompatActivity
 
     private Boolean isDarkThemeOn = true;
 
-    private final String USERNAME = "pigim";
+    private final String USERNAME = "pigim@pigim.pigim";
     private final String PASSWORD = "password";
 
     @Override
@@ -44,16 +49,24 @@ public class LoginActivity extends AppCompatActivity
         }
 
         final Button loginButton = (Button) findViewById(R.id.button_login);
-        final TextView username = (TextView) findViewById(R.id.text_username);
-        final TextView password = (TextView) findViewById(R.id.text_password);
+        final EditText username = (EditText) findViewById(R.id.text_username);
+        final EditText password = (EditText) findViewById(R.id.text_password);
+        final CheckBox showPasswordCheck = (CheckBox) findViewById(R.id.checkBox_show_password);
 
         loginButton.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v)
             {
+                // pattern for email format (https://www.journaldev.com/638/java-email-validation-regex)
+                Pattern emailFormat = Pattern.compile("^[\\w-\\+]+(\\.[\\w]+)*@[\\w-]+(\\.[\\w]+)*(\\.[a-z]{2,})$");
+
+                // first checking if email format
+                Boolean isEmailFormat = username.getText().toString().matches(emailFormat.toString());
+
                 if (username.getText().toString().toLowerCase().equals(USERNAME) &&
-                        password.getText().toString().equals(PASSWORD))
+                        password.getText().toString().equals(PASSWORD) &&
+                        isEmailFormat)
                 {
                     password.setText("");
                     Intent intent = new Intent(LoginActivity.this, MainActivity.class);
@@ -62,8 +75,35 @@ public class LoginActivity extends AppCompatActivity
                 else
                 {
                     password.setText("");
-                    Toast.makeText(LoginActivity.this,
-                            "Username/password is incorrect.\nPlease try again.", Toast.LENGTH_LONG).show();
+                    if (!isEmailFormat)
+                    {
+                        Toast.makeText(LoginActivity.this,
+                                "Username is an invalid email address.\nPlease try again.", Toast.LENGTH_LONG).show();
+                    }
+                    else
+                    {
+                        Toast.makeText(LoginActivity.this,
+                                "Username/password is incorrect.\nPlease try again.", Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+        });
+
+        showPasswordCheck.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
+        {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
+            {
+                // showing password if show password checkbox is checked and setting cursor to the end
+                if(isChecked)
+                {
+                    password.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                    password.setSelection(password.getText().length());
+                }
+                else
+                {
+                    password.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                    password.setSelection(password.getText().length());
                 }
             }
         });
